d7f4772894c6289be260b20e87682a6575045bef
cagdas <cagdasyanar@outlook.com>
2019-03-31 19:40:16 +0300

    Api eklendi

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import SearchBar from './search-bar';
+import CharacterList from './character-list';
+import Details from './details';
+import md5 from 'md5';
+import $ from 'jquery';
 
-import Home from './Home/index';
+const API_URL = 'https://gateway.marvel.com:443/v1/public/';
+const publicKey = '157a244f06e024bd65ee01a768a63e06';
+const privateKey = 'a3ca645123dda0ede92255d7c0673121f88af3e3';
+const ts = '1';
+const auth = `ts=${ts}&apikey=${publicKey}&hash=${md5(`${ts}${privateKey}${publicKey}`)}`;
 
-const App = () => {
-  return (
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
-  );
-};
+class App extends Component {
+  componentDidMount = () => {
+    this.GetInitialChararcters();
+  };
+
+  GetInitialChararcters() {
+    $.getJSON(`${API_URL}/characters?${auth}&limit=5`, result => {
+      const characters = result.data.results;
+      console.log(characters);
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <SearchBar />
+        <CharacterList />
+        <Details />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/character-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterList = props => {
+  return (
+    <div className="col-md-4">
+      <h2> Character List</h2>
+    </div>
+  );
+};
+
+export default CharacterList;
